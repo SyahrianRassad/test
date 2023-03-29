2c1d2d2b5676be3d2866f6ae9b2652f5850aaf37
SyahrianRassad <syahrian_rassad_29rpl@student.smktelkom-mlg.sch.id>
2023-03-29 20:04:43 +0700

    add purgecss5

---
+++ purgecss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    content: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.js'],
+    css: ['./node_modules/bulma/css/bulma.min.css'],
+    output: './src/index.css',
+  };  
