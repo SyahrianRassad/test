c0eba465d5dd43ea7eae1c84b47f2edae64c0360
SyahrianRassad <syahrian_rassad_29rpl@student.smktelkom-mlg.sch.id>
2023-03-30 07:42:46 +0700

    add test14

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./styles/bulma.css";
-import "./styles/slick.css"; 
-import "./styles/slick-theme.css";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
