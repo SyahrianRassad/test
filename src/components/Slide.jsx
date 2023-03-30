1505debc58ef457606486e57042c5da088dc796a
SyahrianRassad <syahrian_rassad_29rpl@student.smktelkom-mlg.sch.id>
2023-03-30 20:06:13 +0700

    add test17

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import Slider from "react-slick";
+import React,{ lazy } from 'react';
 import gambar1 from "../assets/1665626679483.webp"
 import gambar2 from "../assets/1665626814939.webp"
 import gambar3 from "../assets/1665626869182.webp"
 import gambar4 from "../assets/1665626869182.webp"
 import gambar5 from "../assets/1678332186768.webp"
 
+const Slider = lazy(() => import('react-slick'));
 
 const Slide = () => {
   const settings = {
