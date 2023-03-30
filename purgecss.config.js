ade55e3f801f21118f86106d486ba48f5b99cc25
SyahrianRassad <syahrian_rassad_29rpl@student.smktelkom-mlg.sch.id>
2023-03-30 07:28:23 +0700

    add test12

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   content: ['./src/**/*.jsx', './src/**/*.js'],
   css: ['./node_modules/bulma/css/bulma.css'],
-  safelist: ['is-active'], // any classes you want to keep
+  safelist: ['is-active'],
 }
