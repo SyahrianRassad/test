6131fa7616630d621533aa3329fea459db5f022b
SyahrianRassad <syahrian_rassad_29rpl@student.smktelkom-mlg.sch.id>
2023-03-30 22:30:01 +0700

    add test20

--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,4 @@
 module.exports = {
   content: ['./src/**/*.jsx', './src/**/*.js'],
   css: ['./node_modules/bulma/css/bulma.css'],
-  safelist: ['is-active'],
 }
--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,11 +1,10 @@
-import React,{ lazy, Suspense } from 'react';
+import React from 'react'
+import Slider from "react-slick";
 import gambar1 from "../assets/1665626679483.webp"
 import gambar2 from "../assets/1665626814939.webp"
 import gambar3 from "../assets/1665626869182.webp"
 import gambar4 from "../assets/1665626869182.webp"
 import gambar5 from "../assets/1678332186768.webp"
-
-const Slider = lazy(() => import('react-slick'));
 
 const Slide = () => {
   const settings = {
@@ -47,7 +46,6 @@ const Slide = () => {
   };
   return (
     <div className='container mt-2'>
-      <Suspense fallback={<div>Loading...</div>}>
         <Slider {...settings}>
           <div>
             <img src={gambar1} alt="image" />
@@ -65,7 +63,6 @@ const Slide = () => {
             <img src={gambar5} alt="image" />
           </div>
         </Slider>
-      </Suspense>
     </div>
   )
 }
