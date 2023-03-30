8a73fe5763aa31f13500581758de1fe0420d3c9c
SyahrianRassad <syahrian_rassad_29rpl@student.smktelkom-mlg.sch.id>
2023-03-30 08:04:09 +0700

    add test16

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import "bulma/css/bulma.css";
+import "./styles/bulma.css";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
